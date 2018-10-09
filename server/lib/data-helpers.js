2899e871f5f8d02a79f9db2cd615740f8f78795b
John Xu <johnbxu@gmail.com>
2018-10-09 21:31:25 +0000

    changed database to mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,32 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
+
+      callback(null, true);
+
+      MongoClient.connect(MONGODB_URI, (err, database) => {
+        if (err) {
+          console.error(`Failed to connect: ${MONGODB_URI}`);
+          throw err;
+        }
+        console.log(`Connected to mongodb: ${MONGODB_URI}`);
+        database.collection('tweeter').insertOne(newTweet);
+        database.close();
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+      callback(null, db.tweets.sort(sortNewestFirst));
     }
-
   };
-}
+};
--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -3,8 +3,24 @@
 // Requiring a JSON file automatically parses it and returns the data. These
 // are just example tweets to make it less tedious to style the app initially.
 const db = {
-  tweets: require("../data-files/initial-tweets")
-}
+  tweets: {}
+};
 
 module.exports = db;
 
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, database) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  database.collection("tweets").find().toArray((err, tweets) => {
+    db.tweets = tweets;
+    console.log(db);
+    database.close();
+  });
+});
