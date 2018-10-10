a2dd1fc4cf0a79aad95aba8291a934034e66269d
John Xu <johnbxu@gmail.com>
2018-10-10 18:12:48 +0000

    fixed some formatting

--- server/index.js
+++ server/index.js
@@ -8,31 +8,26 @@ const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient   = require("mongodb").MongoClient;
 const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
+const moment        = require("moment");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connects to and reads MongoDB collection in form of array
-const db = {tweets: {}};
-// MongoClient.connect(MONGODB_URI, (err, database) => {
-//   if (err) {
-//     console.error(`Failed to connect: ${MONGODB_URI}`);
-//     throw err;
-//   }
-//   database.collection("tweets").find().toArray((err, tweets) => {
-//     db.tweets = tweets;
-//     database.close();
-//   });
-// });
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  // Helper function that reads/creates tweets from/to database
+  const DataHelpers = require("./lib/data-helpers.js")(db);
 
-// Helper function that reads/creates tweets from/to database
-const DataHelpers = require("./lib/data-helpers.js")(db);
+  // Uses DataHelpers function to send routes to create or read tweets
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Uses DataHelpers function to send routes to create or read tweets
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+});
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,39 +1,23 @@
 "use strict";
 
 // Defines helper functions for saving and getting tweets, using the database `db`
-const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to MongoDB database
     saveTweet: function(newTweet, callback) {
       callback(null, true);
-      MongoClient.connect(MONGODB_URI, (err, database) => {
-        if (err) {
-          console.error(`Failed to connect: ${MONGODB_URI}`);
-          throw err;
-        }
-        database.collection('tweets').insertOne(newTweet);
-        database.close();
-      });
+      db.collection('tweets').insertOne(newTweet);
     },
 
     // Get all tweets in MongoDB database, sorted by newest first
     getTweets: function(callback) {
-      MongoClient.connect(MONGODB_URI, (err, database) => {
-        if (err) {
-          console.error(`Failed to connect: ${MONGODB_URI}`);
-          throw err;
-        }
-        database.collection("tweets").find().toArray((err, tweets) => {
-          let getDB = { tweets: tweets };
-          const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-          callback(null, getDB.tweets.sort(sortNewestFirst));
-          database.close();
-        });
+      let sortedCollection;
+      db.collection('tweets').find().toArray((err, tweets) => {
+        sortedCollection = tweets;
+        const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+        callback(null, sortedCollection.sort(sortNewestFirst));
       });
-    }
+    },
   };
 };
