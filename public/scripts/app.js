6d1c86c71bd7ecc90c6a2af980a70ea19e2a3fb1
John Xu <johnbxu@gmail.com>
2018-10-11 14:10:20 +0000

    changed css to add media query

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 // Creates tweets DOM element
 const createTweetElement = (tweetData) => {
-  const {user, content, created_at} = tweetData;
+  const {user, content, created_at, likes, liked} = tweetData;
   const {avatars, handle, name} = user;
   const time = minutesAgo(created_at);
 
@@ -9,9 +9,11 @@ const createTweetElement = (tweetData) => {
   let $userName = $("<span>").addClass("userName").text(name);
   let $tweetContent = $("<p>").addClass("tweetContent").text(content.text);
   let $timeStamp = $("<p>").addClass("timeStamp").text(time);
+  let $like = $("<i>").addClass("fas fa-heart").addClass('like').data('likes', likes).data('liked', liked);
   let $div = $("<div>").addClass("headerText").append($userName).append($hashTag);
   let $header = $("<header>").append($avatar).append($div);
-  let $footer = $("<footer>").append($timeStamp);
+  let $likesCounter = $('<span>').addClass('likesCounter').text(likes);
+  let $footer = $("<footer>").append($timeStamp).append($like).append($likesCounter);
   let $tweet = $("<article>").addClass("tweet").append($header).append($tweetContent).append($footer);
   return $tweet;
 };
@@ -59,13 +61,22 @@ const ajaxMakeNewTweet = () => {
 };
 
 // Renders tweets currently in database
-const loadTweets = function(){
-  $.get('/tweets').done(function (tweets) {
-    renderTweets(tweets);
-  });
-}();
 
 $(function(){
+  const loadTweets = function(){
+    $.get('/tweets').done(function (tweets) {
+      renderTweets(tweets);
+    }).done(function (){
+      $('.like').each(function(){
+        $(this).attr('data-liked', $(this).data('liked'));
+        console.log($(this).data('liked'));
+      });
+    });
+  }();
+
+
+
+
   // Tracks how many characters are in tweet text area
   $('textarea').keyup(changeLength);
 
@@ -79,6 +90,35 @@ $(function(){
       $(this).siblings('.error').slideUp('fast');
     }
   });
+
+  // PROBLEM: data-liked on DOM is reset on page refresh. This causes problems1
+  $('.tweets').on('click', '.like', (function(event) {
+    if ($(this).attr('data-liked') === 'true') {
+      $(this).attr('data-liked', 'false');
+      $(this).data('liked', false);
+      $(this).data().likes--;
+    } else {
+      $(this).attr('data-liked', 'true');
+      $(this).data('liked', true);
+      $(this).data().likes++;
+    }
+    $.ajax({
+      url: '/tweets/like/',
+      method: 'POST',
+      data: {
+        likes: $(this).data('likes'),
+        liked: $(this).data('liked'),
+        user: $(this).parents('.tweet').find('.userName').text(),
+      }
+    }).done(function(){
+      $(this).siblings('.likesCounter').text($(this).data('likes'));
+    });
+
+    // need to figure out dynamic updating of the counter
+
+    console.log($(this).data('likes'));
+    console.log($(this).data('liked'));
+  }));
 
   // Compose button - toggles Compose Tweet section
   $('#compose').click(function(){
