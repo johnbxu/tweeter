48738fcbdbf90c7de0d4ed73b2284f4fd4a4facb
John Xu <johnbxu@gmail.com>
2018-10-12 14:49:00 +0000

    did some trimming

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -2,8 +2,6 @@
 
 const userHelper      = require('../lib/util/user-helper');
 
-const cookieSession   = require('cookie-session');
-const bcrypt          = require('bcrypt');
 const express         = require('express');
 const tweetsRoutes    = express.Router();
 const methodOverride  = require('method-override');
@@ -11,56 +9,10 @@ const app             = express();
 const Chance          = require('chance');
 const chance          = new Chance();
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['keydonut', 'keyeclair'],
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
-
 app.use(methodOverride('_method'));
 
 module.exports = function(DataHelpers) {
-
-  tweetsRoutes.post('/login', function(req, res) {
-    const user = {  // this should be hashed
-      email: req.body.email,
-      password: req.body.password,
-    };
-    DataHelpers.login(user, (err, response) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(201).send(response);
-        req.session.loggedIn = true;
-        req.session.email = user.email;
-        const variables = {
-          loggedIn: req.session.loggedIn,
-          email: req.session.email,
-        };
-        // res.render('index.html', variables);
-      }
-    });
-  });
-
-  tweetsRoutes.post('/logout', function(req, res) {
-
-  });
-
-  // registeration end point. takes username and password
-  tweetsRoutes.post('/register', function(req, res) {
-    const user = {  // this should be hashed
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
-    };
-    DataHelpers.register(user, (err, response) => {
-      if (err) {
-        res.status(500).json({ error: err });
-      } else {
-        res.status(201).send(response);
-      }
-    });
-  });
-
+  // requests all tweets in the collection and responds with the tweets
   tweetsRoutes.get('/tweets/', function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -71,6 +23,7 @@ module.exports = function(DataHelpers) {
     });
   });
 
+  // posts a tweet with a randomly generated user
   tweetsRoutes.post('/tweets/', function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
@@ -96,6 +49,7 @@ module.exports = function(DataHelpers) {
     });
   });
 
+  // adjusts tweet's 'like' property
   tweetsRoutes.post('/tweets/like/', function(req, res) {
     DataHelpers.updateLikes(req.body.id, req.body.likes, req.body.liked, (err) => {
       if (err) {
